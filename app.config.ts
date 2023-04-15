3a539483fe8887ef812ff70b50d6ea346654b84c
CRBroughton <crbroughton@posteo.uk>
2023-04-15 09:43:05 +0100

    Revert "fix: :bug: remove out card image line"
    This reverts commit 1470dfd0917b91179b38f3458b38e2f4e54b0319.

--- app.config.ts
+++ app.config.ts
@@ -2,12 +2,16 @@ export default defineAppConfig({
   docus: {
     title: 'V3',
     description: 'End-to-end type-safe Nuxt Stack',
+    // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       github: 'crbroughton/V3'
     },
     aside: {
       level: 0,
       exclude: []
+    },
+    header: {
+      // logo: true
     },
     footer: {
       iconLinks: [
